4e328d6871b5b33d7b7c3b8ae2cac0a644303245
Miguel Angel Arenas Correa <miguel.arenas@Miguel-Arenas.local>
2019-06-01 00:16:48 -0500

    First screen on apple watch

--- watch Extension/InterfaceController.swift
+++ watch Extension/InterfaceController.swift
@@ -10,8 +10,32 @@ import WatchKit
 import Foundation
 
 
+public extension WKInterfaceImage {
+    
+    @discardableResult public func setImageWithUrl(url:String, scale: CGFloat = 1.0) -> WKInterfaceImage? {
+        
+        URLSession.shared.dataTask(with: NSURL(string: url)! as URL) { data, response, error in
+            if (data != nil && error == nil) {
+                let image = UIImage(data: data!, scale: scale)
+                
+                DispatchQueue.main.async {
+                    self.setImage(image)
+                }
+            }
+            }.resume()
+        
+        return self
+    }
+}
+
+
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var movieNameLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var imageMovie: WKInterfaceImage!
+    @IBOutlet weak var swipeGestureRecognizer: WKSwipeGestureRecognizer!
+    var swipePosition = 0;
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,11 +45,31 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        let movieName = "Movie \(swipePosition)"
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    
+    override func didAppear() {
+        super.didAppear()
+        swipeGestureRecognizer.direction = [.left, .right]
+    }
+    
+    
+    @IBAction func handleSwipe(_ sender: Any) {
+        if (swipeGestureRecognizer.direction == .left) {
+            swipePosition-=1
+        }else{
+            swipePosition+=1
+        }
+        
+        let movieName = "Movie \(swipePosition)"
+        movieNameLabel.setText(movieName)
+        imageMovie.setImageWithUrl(url : "https://picsum.photos/200/300", scale: 1.0)
+    }
+    
 }
